c1cc11e20c8a7c50a0793b04e81ba3b73d843aeb
siddukumbar22 <siddukumbar3022@gmail.com>
2024-10-16 21:41:37 +0530

    files

---
+++ Resturents.java
@@ -0,0 +1,8 @@
+public class Resturants{
+public static void main(String[] args){
+int Resturants=16;
+System.out.println(Resturants);
+Resturants=19;
+System.out.println(Resturants);
+}
+}
---
+++ Rooms.java
@@ -0,0 +1,8 @@
+public class Rooms{
+public static void main(String[] args){
+int Rooms=12;
+System.out.println(Rooms);
+Rooms=25;
+System.out.println(Rooms);
+}
+}
