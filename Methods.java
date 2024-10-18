6d61d3a25f6d54b92b12971ce99b88a44dd7851b
siddukumbar22 <siddukumbar3022@gmail.com>
2024-10-18 19:45:33 +0530

    java methods

---
+++ Methods.java
@@ -0,0 +1,45 @@
+public class Methods{
+	public static void main(String[] args) {
+
+		speak();
+		run();
+		collage();
+		country();
+		bike();
+		java();
+		processor();
+		cricket();
+
+		}
+				public static void speak(){
+					System.out.println("speak in english");
+				}
+
+				public static void run(){
+					System.out.println("run slowely");
+				}
+
+				public static void collage(){
+					System.out.println("collage is too big");
+				}
+
+				public static void country(){
+					System.out.println("india is my country");
+				}
+
+				public static void bike(){
+					System.out.println("rx bike is excelent");
+				}
+
+				public static void java(){
+					System.out.println("java is intresting");
+				}
+
+				public static void processor(){
+					System.out.println("snapdragon is the fatses processor");
+				}
+				public static void cricket(){
+					System.out.println("kholi is excelent batsman");
+				}
+
+}
---
+++ Save.java
@@ -0,0 +1,15 @@
+public class Save{
+	public static void main(String[] args) {
+
+		speak();
+		run();
+
+		}
+				public static void speak(){
+					System.out.println("speak in english");
+				}
+
+				public static void run(){
+					System.out.println("run slowely");
+				}
+}
